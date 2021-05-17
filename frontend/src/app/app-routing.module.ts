059c105b32229f95b862631f58ccd5a7ce229e3c
jerry-sky <jwroczynski@gmail.com>
2021-05-18 00:09:36 +0200

    frontend: added the ‘about’ page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { AboutComponent } from './about/about.component'
+import { StatsTableComponent } from './stats-table/stats-table.component'
 
-const routes: Routes = []
+const routes: Routes = [
+    {
+        path: '',
+        component: StatsTableComponent,
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/',
+    },
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
