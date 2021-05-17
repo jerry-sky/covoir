05da0caee13b11fb76e437bab568ccdd0af874f0
jerry-sky <jwroczynski@gmail.com>
2021-05-17 11:33:23 +0200

    frontend: added a stub HTTP interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,25 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { MyHttpInterceptor } from './my-interceptor.interceptor'
+
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-    providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+    ],
+    providers: [
+        MyHttpInterceptor,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useExisting: MyHttpInterceptor,
+            multi: true,
+        },
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ frontend/src/app/my-interceptor.interceptor.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing'
+
+import { MyHttpInterceptor } from './my-interceptor.interceptor'
+
+describe('MyHttpInterceptor ', () => {
+    beforeEach(() =>
+        TestBed.configureTestingModule({
+            providers: [MyHttpInterceptor],
+        })
+    )
+
+    it('should be created', () => {
+        const interceptor: MyHttpInterceptor = TestBed.inject(
+            MyHttpInterceptor
+        )
+        expect(interceptor).toBeTruthy()
+    })
+})
